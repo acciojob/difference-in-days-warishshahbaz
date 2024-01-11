1b0531859de37d4a4c634df8bc91f42d6e56082f
warishshahbaz <95901915+warishshahbaz@users.noreply.github.com>
2024-01-11 17:32:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 var dateDiffInDays = function (date1, date2) {
-  //   write your code here
+  let val1  = new Date(date1).getTime();
+   let val2 = new Date(date2).getTime();
+   if(val1 > val2){
+       return -1;
+   }else if(val1 === val2){
+       return 0;
+   }else {
+       let diff = Math.round(( val2 - val1 )/(1000* 3600*24));
+       return diff;
+   }
 };
 
 // Do not change the code below.
